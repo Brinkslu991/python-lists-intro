4e231def4da3fec429698e9bf3eb6e474bb919e5
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2024-10-21 10:54:23 -0400

    rtuyi

---
+++ main.py
@@ -0,0 +1,13 @@
+# Lucas Brinks
+# 10/21/2024
+# List intro
+
+names = ['Josh', 'Josh', 'Zach', 'Chandler']
+
+print(f'My first friend is... {names[0]} not to be confused with {names[1]}. Then my two other friends who I play games with {names[2]} and {names[3]}.')
+
+print(f'{names[0]} is not a real friend he begged to be on the list because he is sad.')
+
+bands = ['Avenged Sevenfold', 'Shinedown', 'Smash Into Pieces']
+
+print(f'My favorite bands are {bands}')
